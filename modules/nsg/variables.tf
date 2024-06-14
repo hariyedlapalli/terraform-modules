ef9d7727e64bf2a6a67bfe649f552ac5a762e42a
hari <hariyedlapalli@gmail.com>
2024-06-14 14:00:04 +0530

    updated RG

---
+++ modules/nsg/variables.tf
@@ -0,0 +1,11 @@
+variable "business_devision" {
+  description = "business devision on large organization"
+  type        = string #list of string #map
+
+}
+
+variable "environment" {
+  description = "environment varaibles used as prefix"
+  type        = string
+
+}
--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,14 +1,17 @@
-resource "azurerm_resource_group" "myrg" { #this is the refrence it will be managed by terraform to store your logs
+/*resource "azurerm_resource_group" "myrg" { #this is the refrence it will be managed by terraform to store your logs
   name = "${local.resource_name_prefix}-${var.resource_group_name}"
   #sap-dev-rg-default
   location = var.resource_group_location
   tags     = local.common_tags
-}
+}*/
+#var.resource_group_name
+#var.resource_group_location
+#var.vnet_name
 resource "azurerm_virtual_network" "vnet" {
   name = "${local.resource_name_prefix}-${var.vnet_name}"
   #this vnet need to be part of your resource group and resource group location
-  location            = azurerm_resource_group.myrg.location
-  resource_group_name = azurerm_resource_group.myrg.name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
   address_space       = var.vnet_address_space
 
   tags = local.common_tags
@@ -16,8 +19,8 @@ resource "azurerm_virtual_network" "vnet" {
 }
 resource "azurerm_subnet" "web_subnet" {
   name                 = "${local.resource_name_prefix}-${var.web_subnet_name}"
-  resource_group_name  = azurerm_resource_group.myrg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.vnet_name
   address_prefixes     = var.web_subnet_address
 }
 locals {
